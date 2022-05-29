4b22b7b4e3994c4d4100a40aac8207331c37fae5
Raddiya <raddiyasultana.612@gmil.com>
2022-05-29 15:10:25 +0600

    firebase add

---
+++ src/Components/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1oK2baBvL7MuGRWiTFYAWKV8vmzUv-XY",
+  authDomain: "final-project12.firebaseapp.com",
+  projectId: "final-project12",
+  storageBucket: "final-project12.appspot.com",
+  messagingSenderId: "148219567800",
+  appId: "1:148219567800:web:e48fa8c9a5d70a81d19e4d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
